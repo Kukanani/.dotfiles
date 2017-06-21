3433ed65df85be8716941c91456d00c898f9fa23
Adam Allevato <adam.d.allevato@gmail.com>
2017-06-20 18:29:43 -0700

    Added gazebo 9 installation

---
+++ bash/gazebo9.bash
@@ -0,0 +1,4 @@
+###################################################################
+# Gazebo latest, and gazebo models directory
+###################################################################
+export GAZEBO_MODEL_PATH=~/osrf/gazebo_models:$GAZEBO_MODEL_PATH
