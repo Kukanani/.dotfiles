ef63cf2ed50d9432bc1479fcfabf6126c8aad6f1
Adam Allevato <Kukanani@users.noreply.github.com>
2017-10-03 22:34:09 -0500

    Niceties

---
+++ bash/kinetic.bash
@@ -0,0 +1,37 @@
+###################################################################
+# ROS
+###################################################################
+# Add your main repo to your path. Only the last item in this list matters, each will overwrite the previous one.
+source /opt/ros/kinetic/setup.bash
+
+# Source the current repo, replacing any previous repo sourcing commands
+alias sourcethis='unset CMAKE_PREFIX_PATH;source ./devel/setup.bash'
+  
+# Automatic return to your last-sourced catkin root directory
+alias roshome='cd $(echo $CMAKE_PREFIX_PATH | cut -d ':' -f 1);cd ..'
+  
+# Automatic catkin_make from any folder (use in conjunction with the above one-liner)
+alias cb='(roshome && exec catkin build --summarize)'
+
+# Automatic catkin_make from any folder (use in conjunction with the above one-liner)
+alias cm='(roshome && exec catkin_make)'
+
+# Automatic catkin_make from any folder (use in conjunction with the above one-liner)
+alias cmi='(roshome && exec catkin_make_isolated --install)'
+
+# Clean catkin_make (delete the generated folders, then catkin_make)
+alias cmclean='(roshome && rm -rf build devel install && cm)'
+
+# Clean catkin_make (delete the generated folders, then catkin_make)
+alias cbclean='(roshome && rm -rf build devel install && cm)'
+
+# Clean catkin_make (delete the generated folders, then catkin_make)
+alias cmiclean='(roshome && rm -rf build_isolated devel_isolated install_isolated && cmi)'
+
+# Change the format of ROS console output. This way you can get rid of the annoying timestamp.
+# The total list of items you can add to this format string is available here. As of the time this article was written, the options were:
+# severity message time thread logger file line function
+export ROSCONSOLE_FORMAT='[${severity}] ${message}'
+
+# Export a package config, helps with finding some manually installed packages
+export PKG_CONFIG_PATH=/home/adam/.local/lib/pkgconfig/:$PKG_CONFIG_PATH
--- install_general.sh
+++ install_general.sh
@@ -4,28 +4,38 @@ if [ -z ${DOTFILE_general_INSTALLED+x} ]; then
   echo "Installing $NAME..."
 ################################################################################
 
-  sudo apt install git gitk mercurial terminator
+sudo apt update
+###################################################################
+# Git
+###################################################################
 
-  # install sublime
-  sudo add-apt-repository ppa:webupd8team/sublime-text-3
-  sudo apt update
-  sudo apt install sublime-text-installer
+sudo apt install git gitk
+git config --global user.name "Adam Allevato"
+git config --global user.email "Kukanani@users.noreply.github.com"
 
-  # install fancy git prompt
-  cd ~
-  git clone https://github.com/magicmonty/bash-git-prompt.git .bash-git-prompt --depth=1
+# install fancy git prompt
+cd ~
+git clone https://github.com/magicmonty/bash-git-prompt.git .bash-git-prompt --depth=1
 
-  echo >> ~/.bashrc
-  echo "export DOTFILE_${NAME}_INSTALLED=yes" >> ~/.bashrc
-  echo "source ~/.dotfiles/bash/general.bash" >> ~/.bashrc
+sudo apt install mercurial terminator unzip sublime-text
 
-  echo "Now that Terminator is installed, you should manually configure it to allow infinite scrollback and disable scroll-on-output."
-  echo "Now that Git is installed, you should generated and register ssh keys by following the instructions at"
-  echo "https://help.github.com/articles/generating-ssh-keys/"
+# install GIF screen capture program
+# PPAs do not exist on 14 or below. Use silentcast (https://github.com/colinkeenan/silentcast)
+sudo add-apt-repository ppa:peek-developers/stable
+sudo apt update
+sudo apt install peek
+
+echo >> ~/.bashrc
+echo "export DOTFILE_${NAME}_INSTALLED=yes" >> ~/.bashrc
+echo "source ~/.dotfiles/bash/general.bash" >> ~/.bashrc
+
+echo "Now that Terminator is installed, you should manually configure it to allow infinite scrollback and disable scroll-on-output."
+echo "Now that Git is installed, you should generated and register ssh keys by following the instructions at"
+echo "https://help.github.com/articles/generating-ssh-keys/"
 ################################################################################
-  echo "Finished installing $NAME"
-  echo "Don't forget to run:"
-  echo ". ~/.bashrc"
+echo "Finished installing $NAME"
+echo "Don't forget to run:"
+echo ". ~/.bashrc"
 else
-  echo "$NAME is already installed."
+echo "$NAME is already installed."
 fi
