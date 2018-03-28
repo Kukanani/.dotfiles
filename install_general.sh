a26a92cb7ad27f21c7c479dd2827fa7b6a69d76e
Adam Allevato <Kukanani@users.noreply.github.com>
2018-03-28 10:43:31 -0500

    Made general installation better

--- install_general.sh
+++ install_general.sh
@@ -1,15 +1,9 @@
-#!/bin/bash
-NAME=general
-if [ -z ${DOTFILE_general_INSTALLED+x} ]; then
-  echo "Installing $NAME..."
-################################################################################
-
-sudo apt update
+#/bin/bash
 ###################################################################
 # Git
 ###################################################################
-
-sudo apt install git gitk
+sudo apt-get update
+sudo apt-get install -y git
 git config --global user.name "Adam Allevato"
 git config --global user.email "Kukanani@users.noreply.github.com"
 
@@ -17,25 +11,13 @@ git config --global user.email "Kukanani@users.noreply.github.com"
 cd ~
 git clone https://github.com/magicmonty/bash-git-prompt.git .bash-git-prompt --depth=1
 
-sudo apt install mercurial terminator unzip sublime-text
-
-# install GIF screen capture program
-# PPAs do not exist on 14 or below. Use silentcast (https://github.com/colinkeenan/silentcast)
-sudo add-apt-repository ppa:peek-developers/stable
-sudo apt update
-sudo apt install peek
-
-echo >> ~/.bashrc
-echo "export DOTFILE_${NAME}_INSTALLED=yes" >> ~/.bashrc
-echo "source ~/.dotfiles/bash/general.bash" >> ~/.bashrc
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt-get install -y apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install -y mercurial terminator unzip sublime-text gitk
 
 echo "Now that Terminator is installed, you should manually configure it to allow infinite scrollback and disable scroll-on-output."
-echo "Now that Git is installed, you should generated and register ssh keys by following the instructions at"
-echo "https://help.github.com/articles/generating-ssh-keys/"
 ################################################################################
-echo "Finished installing $NAME"
 echo "Don't forget to run:"
 echo ". ~/.bashrc"
-else
-echo "$NAME is already installed."
-fi
