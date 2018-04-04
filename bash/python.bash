ccfbdc8586e88e7132b548692165de0f0d44d8d6
Adam Allevato <Kukanani@users.noreply.github.com>
2018-04-03 21:26:10 -0500

    Added virtualenvwrapper source call

--- bash/python.bash
+++ bash/python.bash
@@ -2,6 +2,4 @@
 # Pip/Python/Virtual Environments
 ###################################################################
 # virtualenv
-source /usr/local/bin/virtualenvwrapper.sh
-# autoenv
-source /usr/local/bin/activate.sh
+source /usr/share/virtualenvwrapper/virtualenvwrapper.sh
