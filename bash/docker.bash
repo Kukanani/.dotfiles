78b4e1b5efe011355d45f8cc6e3d8a5823a4e7dc
Adam Allevato <39279961+adam-diligent@users.noreply.github.com>
2018-08-20 11:53:51 -0500

    Better docker commands!

--- bash/docker.bash
+++ bash/docker.bash
@@ -3,5 +3,5 @@
 ###################################################################
 # via https://forums.docker.com/t/how-to-delete-cache/5753
 alias docker_clean_images='docker rmi $(docker images -a --filter=dangling=true -q)'
-alias docker_kill_stopped='docker rm $(docker ps --filter=status=exited --filter=status=created -q)'
+alias docker_kill_all='docker rm $(docker ps --filter=status=exited --filter=status=created -q)'
 alias docker_run_gui='XSOCK=/tmp/.X11-unix && XAUTH=/tmp/.docker.xauth && xauth nlist :0 | sed -e "s/^..../ffff/" | xauth -f $XAUTH nmerge - && docker run  -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH  -e DISPLAY=$DISPLAY'
