209f09e8c892e4ea3c6ae038bf0bc5635f5c8ff5
Adam Allevato <Kukanani@users.noreply.github.com>
2019-01-09 21:03:06 -0600

    Added installer for cool document editors

---
+++ install_docs.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+sudo apt-get update
+
+# Document utilities
+sudo apt-get install -y xournal pdfshuffler
+
+# Joplin for taking notes
+wget -O - https://raw.githubusercontent.com/laurent22/joplin/master/Joplin_install_and_update.sh | bash
+
+# PDF cropper
+snap install krop
+
+################################################################################
+echo "Don't forget to run:"
+echo ". ~/.bashrc"
